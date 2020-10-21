b27965433da4ee5ec93a30096d23c7c663072525
yesminee <43929137+yesminee@users.noreply.github.com>
2020-10-21 13:57:42 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//written in jquary syntaxe
+$(document).ready(function () {
+    $('[data-toggle=tooltip"]').tooltip();
+}); //whenever there is an att of this kind for html tag turn onthe tooltip
+
+//controler le comportement des bouttons
+$(document).ready(function () {
+    $('#mycarousel').carousel({ interval: 2000 });  //sliding speed
+
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {  //if this button has an fa-pause class
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');    //We're going to remove the pause class from that and then, we're going to add the playClass to restart sliding
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')) {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');
+        }
+    });
+});
